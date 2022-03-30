89f9ffd181f0b4bef094d2b1ad64afe5cb9be741
vit19988 <Mr.peerawit@live.com>
2022-03-30 22:55:40 +0700

    generate getter and setter

--- test-git/src/Dog.java
+++ test-git/src/Dog.java
@@ -4,6 +4,18 @@ public class Dog {
 	private String color;
 	public Dog() {
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
 	
 	
 	
